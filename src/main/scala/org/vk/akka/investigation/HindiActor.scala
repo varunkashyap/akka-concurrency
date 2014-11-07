9bd34af7484f1eec76602216df6238fa2147ff14
Varun Kashyap <varunkashyapcse@gmail.com>
2014-11-06 16:26:30 -0800

    Basic hindi actor, just a simple send receive

---
+++ src/main/scala/org/vk/akka/investigation/HindiActor.scala
@@ -0,0 +1,29 @@
+package org.vk.akka.investigation
+
+import akka.actor.{Actor, Props, ActorSystem}
+
+class HindiActor extends Actor {
+  def receive = {
+    case "Good Morning" =>
+      println ("Actor: Shubh Prabhat")
+    case "How are you?" =>
+      println ("Actor: Main theek hun, aap kaise ho?")
+  }
+}
+
+object HindiActorMain {
+  val system = ActorSystem("HindiActor")
+  val actor = system.actorOf(Props[HindiActor], "Shrimaan")
+
+  def send(msg: String) = {
+    println(s"Me: $msg")
+    actor ! msg
+    Thread.sleep(100)
+  }
+
+  def main(args: Array[String]) {
+    send("Good Morning")
+    send("How are you?")
+    system.shutdown()
+  }
+}
